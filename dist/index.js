540d1e9266e7358b51ac684e922946944d76d1ea
Owen Rumney <owen@owenrumney.co.uk>
2024-06-16 13:05:41 +0100

    fix: remove node_modules

--- dist/index.js
+++
@@ -1,2 +0,0 @@
-import commit from './git.js';
-await commit();
--- src/git.ts
+++ src/git.ts
@@ -1,42 +1,70 @@
 import inquirer from 'inquirer';
-import { exec } from 'child_process';
-
+import confirm from '@inquirer/confirm';
+import { exec, spawn } from 'child_process';
 
 const commieTheme = {
-  prefix : ''
-}
+  prefix: '',
+};
 
 async function attemptResolveBranchName(): Promise<string> {
-  exec('git rev-parse --abbrev-ref HEAD', (_, stdout, __) => {
-    if (stdout) {
-      return stdout.replaceAll('-', ' ');
-    }
+  return new Promise<string>((resolve, reject) => {
+    exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
+      if (stdout) {
+        resolve(stdout.trim());
+      } else {
+        resolve('');
+      }
+    });
   });
-  return '';
 }
 
 export default async function commit() {
   const branchName = await attemptResolveBranchName();
+  console.log('branchName', branchName);
+  const commitMessage = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'type',
+      message: 'Commit type?',
+      choices: ['feat', 'fix', 'chore', 'enhance', 'refactor', 'test', 'docs'],
+      default: 'feat',
+      theme: {
+        prefix: commieTheme,
+      },
+    },
+    {
+      type: 'input',
+      name: 'message',
+      default: branchName,
+      message: 'Commit message',
+      theme: commieTheme,
+    },
+  ]);
 
-  const answers = inquirer.prompt(
-    [
-      {
-        type: 'list',
-        name: 'type',
-        message: 'Commit type?',
-        choices: ['feat', 'fix', 'chore', 'enhance', 'refactor', 'test', 'docs'],
-        default: 'feat',
-        theme: {
-          prefix: commieTheme,
+  const confirmCommit = await confirm({message: 'Are you sure you want to commit?'});
+  if (confirmCommit) {
+    exec(
+      `git commit -m "${commitMessage.type}: ${commitMessage.message}"`,
+      (err, _stdout) => {
+        if (err) {
+          console.error(err);
+          return process.exit(1);
         }
       },
-      {
-        type: 'input',
-        name: 'message',
-        default: branchName,
-        message: 'Commit message',
-        theme: commieTheme
-      },
-    ],
-  );
+    );
+  }
+
+  const addMessage = await confirm({message: 'Add a body to the commit?'});
+  if (addMessage) {
+const shell = spawn('git',['commit', '--amend'], { stdio: 'inherit' })
+shell.on('exit', (code: number) => {
+  if (code === 0) {
+    console.log('Commit successful!')
+  } else {
+    console.error('Commit failed')
+  }
+})
+  }
+
+  
 }
